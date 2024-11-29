016f5eede62291838fe783fcc9eb45692d542c22
Ishan <ishandotsh@gmail.com>
2024-11-28 20:32:05 -0800

    prelim tests+ structure

--- inference.py
+++
@@ -1,2 +0,0 @@
-def predict_stocks(date):
-    pass
---
+++ smarttrader/inference.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import datetime as dt
+from datetime import date
+import matplotlib.pyplot as plt
+import yfinance as yf
+import numpy as np
+
+def predict_stocks(date):
+    pass
